8a38146dd6e3cd0e4d8d7915beb0fab43c01d64b
Magimart <magmafelix2002@yahoo.com>
2022-01-05 10:14:44 +0100

    client config private

--- src/client.js
+++ src/client.js
@@ -1,15 +1,10 @@
+
 import * as contentful from 'contentful'
 
   export const client = contentful.createClient({
-    space: '44cqsw49kvuq',
-    accessToken: 'vfDSz7RVHCgRn1YoBxasiY_JTPoiXRyngj-Ri1mG_oI'
+    space: process.env.space,
+    accessToken: process.env.accessToken
   })
 
 
-// import * as contentful from 'contentful'
-// import * as contentful from 'contentful-management'
 
-// export const client = contentful.createClient({
-//     space: '44cqsw49kvuq',
-//     accessToken: 'YOUR_ACCESS_TOKEN',
-// })
