98bc1c4128566b5414c6b8bfd3cb98a05c15f3a0
Magimart <magmafelix2002@yahoo.com>
2022-01-25 13:49:39 +0100

    remove histroy config

--- src/components/backed.js
+++ src/components/backed.js
@@ -12,7 +12,6 @@ const App=()=> {
 
   const [recipes, setRecipes]=useState();  //[]
 
-  // const [updateRecipes, setUpdateRecipes]=useState();  //[]
 
   useEffect(()=>{
 
@@ -34,56 +33,6 @@ const App=()=> {
     
   }, [ ]);
 
-    //--collection or space id: 44cqsw49kvuq
-    // rolex entry id: 6TsboYUBGHOZbsPtg0NUnx
-    // tocken Name: updatingTestforFelixapp
-    // access token:   CFPAT-ofdS8OgEVpEO3UlLnbZbwvYvjkEUgaSOrRdl4k5znYI
-            //  You should always update resources in the following order:
-            //  Fetch current resource.
-            //  Make changes to the current resource.
-            //  Update the resource by passing the changed resource along with current version number.
-
-
-  // updating data
-           // Pseucode------------------
-           // user clicks on the image-- fetch data function is trigered
-           // checks if stored data same as clicked data, if true return a form
-           // then connects to contenful updates the data
-           // publish the data
-
-  // useEffect(()=>{
-      //  async function updateConnectMyContent() { 
-
-      //     let space = await client.getSpace('44cqsw49kvuq');
-      //     return  await space.getEnvironment('master') ; 
-      //   }
-
-      //    // async call back to return the updateConnect
-      //   (async () =>  {  
-      //       let environment = await updateConnectMyContent();
-      //       console.log(environment); 
-      //    }
-      //    )();
-
-
-
-    //  client.getSpace('44cqsw49kvuq').then((space) => {
-    //   // Now that we have a space, we can get entries from that space
-    //   space.getEntries().then((entries) => {
-    //     console.log(entries.items)
-    //   })
-    
-      // let's get a content type
-    //   space.getContentType('product').then((contentType) => {
-    //     // and now let's update its name
-    //     contentType.name = 'New Product'
-    //     contentType.update().then((updatedContentType) => {
-    //       console.log('Update was successful')
-    //     })
-    //   })
-    // })
-    
-  // }, [ ]);
 
 
   return (
--- src/components/client.js
+++ src/components/client.js
@@ -1,8 +1,8 @@
 import * as contentful from 'contentful'
 
   export const client = contentful.createClient({
-    space: '44cqsw49kvuq',
-    accessToken: 'vfDSz7RVHCgRn1YoBxasiY_JTPoiXRyngj-Ri1mG_oI'
+    space: process.env.space,
+    accessToken: process.env.accessToken
   })
 
 
